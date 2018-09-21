17d2f030e067ee61ed3cb9190263794a6afd3e34
thraneh <thraneh@gmail.com>
2018-09-21 07:09:59 +0200

    proper init of submodule

--- update.sh
+++ update.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
 git submodule update --init --recursive
+
+function pull {
+  (
+    echo -e "\033[1;34m--- $2 ---\033[0m" && \
+    cd $2 && \
+    git fetch --all && \
+    git checkout $1 && \
+    git pull origin HEAD
+  )
+}
+
+pull master roq-ansible 
+
+echo -e "\033[1;32mDone!\033[0m"
