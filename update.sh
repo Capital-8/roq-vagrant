2d0bcac468ef8d5ad912dc75e297bb2486c46949
thraneh <thraneh@gmail.com>
2018-09-21 07:11:16 +0200

    better

--- update.sh
+++ update.sh
@@ -1,17 +1,4 @@
 #!/usr/bin/env bash
 
 git submodule update --init --recursive
-
-function pull {
-  (
-    echo -e "\033[1;34m--- $2 ---\033[0m" && \
-    cd $2 && \
-    git fetch --all && \
-    git checkout $1 && \
-    git pull origin HEAD
-  )
-}
-
-pull master roq-ansible 
-
-echo -e "\033[1;32mDone!\033[0m"
+git submodule foreach --recursive git checkout master
